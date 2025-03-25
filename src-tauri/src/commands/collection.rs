b3984aea3be8bb6088ac24b3c3251b734f637b59
GintoBos <114522869+GintoBos@users.noreply.github.com>
2025-03-25 21:45:02 +0100

    issue: now you can create directories but not inside the project directory lol hahaha

--- src-tauri/src/commands/collection.rs
+++ src-tauri/src/commands/collection.rs
@@ -1,15 +1,23 @@
+use tauri;
 use std::fs;
 use std::path::Path;
+
 //creates a new subdirectory in the song directory (for the create new collection button)]
-use tauri;
+
 #[tauri::command(rename_all = "snake_case")]
-pub fn create_song_directory(name : String) -> Result<(), String>{
-    let path = format!("src/routes/songs/{name}");
-    let my_path = Path::new(&path);
+pub fn create_song_directory(name: String) -> Result<(), String> {
+    let path = format!("../../../src/songs/{name}");
+
+    if name.is_empty() {
+        return Err("Collection name cannot be empty".into());
+    }
     
+    let my_path = Path::new(&path);
+
     match my_path.exists() {
         true => return Ok(()),
         false => fs::create_dir(my_path).map_err(|err| err.to_string())?,
     }
+
     Ok(())
 }
