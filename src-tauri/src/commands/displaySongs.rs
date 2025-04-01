e7a1e70a5d8d65f571fd8f3e734bf4732d7a8831
GintoBos <114522869+GintoBos@users.noreply.github.com>
2025-04-01 22:03:43 +0200

    added some comments for fun

--- src-tauri/src/commands/displaySongs.rs
+++ src-tauri/src/commands/displaySongs.rs
@@ -9,11 +9,11 @@ use serde::Serialize;
 use tauri::command;
 
 #[derive(Serialize)]
-struct Song_Info {
+struct SongInfo {
     path: String,
     song_title: Option<String>,
     song_artist: Option<String>,
-    song_duration: Option<u64>, // duration in seconds
+    song_duration: Option<u64>, 
     song_cover_path: Option<String>,
 }
 
@@ -25,17 +25,19 @@ fn read_audio_folder(audio_path: String, cover_output_dir: String) -> Vec<SongIn
     let cover_dir = PathBuf::from(&cover_output_dir);
     let _ = create_dir_all(&cover_dir);
 
+    //iterating through the audio_path
     for entry in WalkDir::new(&audio_path).into_iter().filter_map(|e| e.ok()) {
         let path = entry.path();
+        //checks the files extension 
         if path.extension().map_or(false, |ext| ext == "mp3" || ext == "flac" || ext == "m4a") {
             if let Ok(file) = File::open(path) {
-                let mss = MediaSourceStream::new(Box::new(file), Default::default());
+                let mut mss = MediaSourceStream::new(Box::new(file), Default::default());
                 let mut hint = Hint::new();
                 if let Some(ext) = path.extension().and_then(|e| e.to_str()) {
                     hint.with_extension(ext);
                 }
 
-                if let Ok(probed) = symphonia::default::get_probe().format(&hint, mss, &MetadataOptions::default(), Default::default()) {
+                if let Ok(probed) = symphonia::default::get_probe().format(&hint, mss, &MetadataOptions::default(), &Default::default()) {
                     let format = probed.format;
                     let metadata = format.metadata().current();
                     let title = metadata.get_string("title").map(|s| s.to_string());
@@ -55,7 +57,6 @@ fn read_audio_folder(audio_path: String, cover_output_dir: String) -> Vec<SongIn
                             cover_path = Some(cover_filepath.to_string_lossy().into());
                         }
                     }
-
                     songs.push(Song_Info {
                         path: path.to_string_lossy().into(),
                         song_title,
